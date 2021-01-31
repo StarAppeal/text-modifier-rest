68e6e126ae2e7990cd4dee0d80b2c1923eda6320
tsomic <keller.nils99@gmail.com>
2021-01-31 16:09:51 +0100

    adds spermi

--- src/ifies/index.js
+++ src/ifies/index.js
@@ -1,9 +1,11 @@
 let owo = require("./owo");
 let winzi = require("./tiny");
 let emoji = require("./emoji");
+let spermi = require("./spermi");
 
 module.exports = {
   owo: owo,
   winzi: winzi,
   emoji: emoji,
+  spermi: spermi,
 };
---
+++ src/ifies/spermi.js
@@ -0,0 +1,5 @@
+var fizieren = function (str) {
+  return str;
+};
+
+module.exports = fizieren;
