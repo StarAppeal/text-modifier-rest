74b6ab5796d55ba9c3e816607dd08c7fb04e3464
tsomic <keller.nils99@gmail.com>
2021-03-07 18:02:55 +0100

    adds reverse param to braille

--- src/ifies/braille.js
+++ src/ifies/braille.js
@@ -1,11 +1,24 @@
 const br = require("braille");
 
 var fizieren = function (str, params) {
-  return br.toBraille(str);
+  const reverse =
+    params && params.reverse !== undefined ? params.reverse : false;
+  str = str
+    .toUpperCase()
+    .replace(/Ä/g, "AE")
+    .replace(/Ö/g, "OE")
+    .replace(/Ü/g, "UE");
+  return reverse ? br.toText(str) : br.toBraille(str);
 };
 
 module.exports = {
   function: fizieren,
   name: "Braillify",
   show: true,
+  params: [
+    {
+      name: "reverse",
+      type: "Boolean",
+    },
+  ],
 };
