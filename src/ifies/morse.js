b1312b1a486b9da6bc203e98210eb7d64cbce3f6
tsomic <keller.nils99@gmail.com>
2021-03-07 18:28:56 +0100

    adds reverse to morse

--- src/ifies/morse.js
+++ src/ifies/morse.js
@@ -59,17 +59,36 @@ var morseCode = {
 };
 
 var fizieren = function (str, params) {
-  return str
-    .toLowerCase()
-    .split("")
-    .map((el) => {
-      return morseCode[el] ? morseCode[el] : el;
-    })
-    .join(" ");
+  const reverse =
+    params && params.reverse !== undefined ? params.reverse : false;
+  console.log(str.split(" "));
+  return reverse
+    ? str
+        .split(" ")
+        .map((el) => {
+          return el === ""
+            ? " "
+            : Object.keys(morseCode).find((key) => morseCode[key] === el);
+        })
+        .join("")
+        .replace(/  /g, " ")
+    : str
+        .toLowerCase()
+        .split("")
+        .map((el) => {
+          return morseCode[el] ? morseCode[el] : el;
+        })
+        .join(" ");
 };
 
 module.exports = {
   function: fizieren,
   name: "Morsify",
   show: true,
+  params: [
+    {
+      name: "reverse",
+      type: "Boolean",
+    },
+  ],
 };
