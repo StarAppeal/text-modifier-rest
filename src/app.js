09a1db1188fd7d49a9ee8837e47a52ac24da9b43
StarAppeal <info@starappeal.app>
2021-03-09 22:11:32 +0100

    cors test

--- src/app.js
+++ src/app.js
@@ -7,7 +7,7 @@ var application = express();
 
 app.all('/*', function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "X-Requested-With");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
 
