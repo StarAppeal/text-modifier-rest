2d3cfd463b258febcac9669b30d7f38ebf25c245
StarAppeal <silkfan11@gmail.com>
2021-03-09 22:27:25 +0100

    cors test

--- src/app.js
+++ src/app.js
@@ -6,8 +6,10 @@ var fier = require("./routes/fier");
 var application = express();
 
 application.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
-  next();
+	res.header('Access-Control-Allow-Origin', '*');
+	res.header("Access-Control-Allow-Methods", "GET,POST");
+	res.header("Access-Control-Allow-Headers", "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers");
+	next();
 });
 
 application.use(express.json());
