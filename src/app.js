711e3eda65d35be6ed8651f1cf7a9d2bda430c7b
StarAppeal <info@starappeal.app>
2021-03-09 22:08:52 +0100

    deleted package

--- src/app.js
+++ src/app.js
@@ -1,13 +1,15 @@
 var express = require("express");
 var path = require("path");
 
-var cors = require('cors')
-
 var fier = require("./routes/fier");
 
 var application = express();
 
-application.use(cors({credentials: true, origin: true}));
+app.all('/*', function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "X-Requested-With");
+  next();
+});
 
 application.use(express.json());
 
