0281368a68bd3bfc20bb58770d877b6ea89d3b0e
StarAppeal <info@starappeal.app>
2021-03-09 22:07:09 +0100

    add cors for testing

--- src/app.js
+++ src/app.js
@@ -1,9 +1,13 @@
 var express = require("express");
 var path = require("path");
 
+var cors = require('cors')
+
 var fier = require("./routes/fier");
 
 var application = express();
+
+application.use(cors({credentials: true, origin: true}));
 
 application.use(express.json());
 
