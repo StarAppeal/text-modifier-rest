cff48e063a88b843606d08502cb89e33c4258869
StarAppeal <info@starappeal.app>
2021-03-09 22:24:10 +0100

    fixed app to application

--- src/app.js
+++ src/app.js
@@ -5,7 +5,7 @@ var fier = require("./routes/fier");
 
 var application = express();
 
-app.use((req, res, next) => {
+application.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*');
   next();
 });
