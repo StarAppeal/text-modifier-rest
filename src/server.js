c437ff5f1291ab33fea3f093c718f814e9d8c3e2
StarAppeal <silkfan11@gmail.com>
2021-03-09 22:24:10 +0100

    fixed app to application

--- src/server.js
+++ src/server.js
@@ -1,7 +1,7 @@
 const app = require("./app");
 const http = require("http");
 
-var port = process.argv[2] === "DEBUG" ? 80 : process.env.PORT;
+var port = process.argv[2] === "DEBUG" ? 81 : process.env.PORT;
 
 http.createServer(app).listen(port, function () {
   console.log("App listens on port: " + this.address().port);
