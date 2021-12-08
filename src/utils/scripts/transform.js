b1f15cfd0bb62f2a1c37c7798f03ff4b0f311a72
StarAppeal <silkfan11@gmail.com>
2021-12-08 23:41:12 +0100

    adding transforming script

---
+++ src/utils/scripts/transform.js
@@ -0,0 +1,33 @@
+const fs = require("fs");
+
+let config = {
+  inputFile: process.env.npm_config_inputFile,
+  outputFile: process.env.npm_config_outputFile,
+};
+
+console.log("inputFile: " + config.inputFile);
+console.log("outputFile: " + config.outputFile);
+
+console.log("Reading inputfile...");
+let content = JSON.parse(fs.readFileSync(config.inputFile, "utf8"));
+
+console.log("transforming to correct syntax...");
+let result = transform(content);
+
+console.log("writing file");
+fs.writeFileSync(config.outputFile, JSON.stringify(result));
+
+function transform(input) {
+  let result = {};
+  for (let prop in input) {
+    if (prop.includes(",")) {
+      let s = prop.split(",");
+      for (let i = 0; i < s.length; i++) {
+        result[s[i]] = input[prop];
+      }
+    } else {
+      result[prop] = input[prop];
+    }
+  }
+  return result;
+}
