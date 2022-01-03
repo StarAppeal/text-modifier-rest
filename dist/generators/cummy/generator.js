d0e97d5bdac70497b2a15c733969aff418b1db6c
tsomic <keller.nils99@gmail.com>
2022-01-03 02:31:42 +0100

    remove dist folder

--- dist/generators/cummy/generator.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-function generator(request) {
-    return request.text;
-}
-exports.default = generator;
-//# sourceMappingURL=generator.js.map
