782ecb08e0c2a5bef840f9be3f56cc1d0ee98fa3
Nils Keller <60610467+tsomic@users.noreply.github.com>
2023-02-13 23:53:49 +0100

    #22: add strikethrough (#29)
    * #22: add strikethrough
    
    * #22: apply review suggestion
    
    * #22: improve comment

---
+++ src/modifiers/strikethrough/config.ts
@@ -0,0 +1,10 @@
+import ModifierConfig from "@/constants/interfaces/ModifierConfig";
+import modify from "./modifier";
+
+const config: ModifierConfig = {
+  function: modify,
+  label: "S̶t̶r̶i̶k̶e̶t̶h̶r̶o̶u̶g̶h̶̶",
+  value: "strikethrough",
+};
+
+export default config;
