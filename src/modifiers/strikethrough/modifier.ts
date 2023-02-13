ca82aa88cbe6e0d290071e394f8ce24f53c40b1e
tsomic <keller.nils99@gmail.com>
2023-02-13 23:52:53 +0100

    #22: improve comment

--- src/modifiers/strikethrough/modifier.ts
+++ src/modifiers/strikethrough/modifier.ts
@@ -4,6 +4,6 @@ const STRIKETHROUGH_CHAR = String.fromCharCode(822);
 
 export default function modify(request: ModifierRequest) {
   const matches = request.text.match(/./g);
-  // strike must be after every char, and join just adds it in between. So we must add one to the end
+  // strike must be after every char, and join just adds it in between. So we have to add one to the end
   return matches.join(STRIKETHROUGH_CHAR) + STRIKETHROUGH_CHAR;
 }
